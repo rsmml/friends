608c54c377f2dc57fa41eb1e1b0c75770e9b973a
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2021-03-24 11:41:23 +0100

    Validation | Module Movies and Characters created with a 1:n relationship + migration done

--- lib/friends/person.ex
+++ lib/friends/person.ex
@@ -1,5 +1,18 @@
 defmodule Friends.Person do
   use Ecto.Schema
+  import Ecto.Changeset
+
+  # def changeset(struct, params) do
+  #   struct
+  #   |> cast(params, [:name, :age])
+  # end
+
+  def changeset(struct, params) do
+    struct
+    |> cast(params, [:name, :age])
+    |> validate_required([:name])
+    |> validate_length(:name, min: 2)
+  end
 
   schema "people" do
     field :name, :string
